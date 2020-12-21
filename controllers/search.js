0aa0c4449b2fffd99bc923d8dac7b8e46241b7b3
Toon Bosmans <toon.j.bosmans@gmail.com>
2020-12-21 14:39:47 -0600

    included route for external API search

---
+++ controllers/search.js
@@ -0,0 +1,43 @@
+const { Router } = require("express");
+const router = Router();
+require("dotenv").config();
+const fetch = require("node-fetch");
+const { API_KEY } = process.env;
+
+//this is a TEST for the Redis caching**
+const redis = require("redis");
+const redisPort = 6379;
+const client = redis.createClient(redisPort);
+client.on("error", (err) => console.log(err));
+
+router.get("/:id", (req, res) => {
+  const searchTerm = req.params.id;
+  try {
+    client.get(searchTerm, async (err, results) => {
+      if (err) throw err;
+
+      if (results) {
+        res.status(200).send({
+          results: JSON.parse(results),
+          message: "data retrieved from the cache",
+        });
+      } else {
+        fetch(
+          `https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${searchTerm}&apikey=${API_KEY}`
+        )
+          .then((response) => response.json())
+          .then((data) => {
+            client.setex(searchTerm, 600, JSON.stringify(data));
+            res.status(200).send({
+              results: data,
+              message: "cache miss",
+            });
+          });
+      }
+    });
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+  }
+});
+
+module.exports = router;
--- index.js
+++ index.js
@@ -30,6 +30,7 @@ const app = express();
 //OTHER IMPORTS
 const morgan = require("morgan");
 const holdingRouter = require("./controllers/holding");
+const searchRouter = require("./controllers/search");
 
 ////////////
 //MIDDLEWARE
@@ -41,44 +42,9 @@ app.use(morgan("tiny")); //logging
 ///////////////
 //Routes and Routers
 //////////////
-//this is a TEST for the Redis caching**
-const redis = require("redis");
-const redisPort = 6379;
-const client = redis.createClient(redisPort);
-client.on("error", (err) => console.log(err));
-
-app.get("/test/:id", (req, res) => {
-  const searchTerm = req.params.id;
-  try {
-    client.get(searchTerm, async (err, results) => {
-      if (err) throw err;
-
-      if (results) {
-        res.status(200).send({
-          results: JSON.parse(results),
-          message: "data retrieved from the cache",
-        });
-      } else {
-        fetch(
-          `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${searchTerm}&apikey=${API_KEY}`
-        )
-          .then((response) => response.json())
-          .then((data) => {
-            client.setex(searchTerm, 600, JSON.stringify(data));
-            res.status(200).send({
-              results: data,
-              message: "cache miss",
-            });
-          });
-      }
-    });
-  } catch (err) {
-    res.status(500).send({ message: err.message });
-  }
-});
 
 app.use("/auth", AuthRouter);
-
+app.use("/search", searchRouter);
 app.use(
   "/holdings",
   //auth,
